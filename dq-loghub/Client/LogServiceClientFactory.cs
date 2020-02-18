e166eea8173ecf217c98580a0ee565cae3cc2347
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-02-18 18:12:06 +0800

    添加项目文件。

---
+++ dq-loghub/Client/LogServiceClientFactory.cs
@@ -0,0 +1,33 @@
+﻿using Aliyun.Api.LogService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dq_loghub.Client
+{
+    public class LogServiceClientFactory
+    {
+        //private static string endpoint = "cn-hangzhou.log.aliyuncs.com";
+        //private static string projectName = "net-project-test";
+        //private static string accessKeyId = "****";
+        //private static string accessKeySecret = "****";
+
+        private static string endpoint = "<你的外网域名>";
+        private static string projectName = "<你的项目名称>";
+        private static string accessKeyId = "<访问阿里云API的密钥Id>";
+        private static string accessKeySecret = "<访问阿里云API的密钥Secret>";
+        private static string logstore = "<日志库名称>";
+
+
+        public static ILogServiceClient BuildSimpleClient()
+        {
+            return LogServiceClientBuilders.HttpBuilder
+                // 服务入口<endpoint>及项目名<projectName>。
+                .Endpoint(endpoint, projectName)
+                // 访问密钥信息。
+                .Credential(accessKeyId, accessKeySecret)
+                .Build();
+        }
+    }
+}
---
+++ dq-loghub/Controllers/LogController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aliyun.Api.LogService;
+using Aliyun.Api.LogService.Domain.Log;
+using dq_loghub.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Quartz;
+
+namespace dq_loghub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        // GET: api/Log
+        [HttpGet]
+        public String Get()
+        {
+            // 1. 构建 `ILogServiceClient` 实例：
+            var client = LogServiceClientFactory.BuildSimpleClient();
+
+            // 2.查询相应日志库中的日志数据
+            string logstore = "net-logstore";
+            var asyncTask = client.GetLogsAsync
+           (
+               // 「必填参数」作为方法的普通必须参数。
+               logstore,
+               //查询的时间区间
+               DateBuilder.DateOf(11, 45, 30, 1, 1, 2018),
+               DateTimeOffset.Now,
+               
+               // 「可选参数」作为方法的可选参数，可通过命名参数方式指定。
+               offset: 1,
+               line: 100
+           );
+            // 在普通控制台的环境下同步等待结果直接调用即可。
+            var response = asyncTask.Result;
+
+            var result = response
+                          // 此方法会确保返回的响应失败时候抛出`LogServiceException`。
+                          .EnsureSuccess()
+                          // 此处获取Result是安全的。
+                          .Result;
+            //3.直接输出Log信息
+            Console.WriteLine($"RequestId：{response.RequestId}");
+            Console.WriteLine($"日志总数：{result.Count}");
+            Console.WriteLine($"首条日志：{result.Logs.FirstOrDefault()}");
+
+            //直接返回String数据 方便测试
+            return $"RequestId：{response.RequestId}" + $"日志总数：{result.Count}" + $"首条日志：{result.Logs.FirstOrDefault()}";
+
+        }
+
+
+
+        // POST: api/Log
+        [HttpPost]
+        public string Post()
+        {
+            // 1. 构建 `ILogServiceClient` 实例：
+            var client = LogServiceClientFactory.BuildSimpleClient();
+
+            // 2.模拟Log日志
+            // 注意!!此处时间为LogHub查询时对应的时间
+            var rawLogs = new[]
+            {
+                    "2020-02-17 12:34:56 INFO id=1 status=foo",
+                    "2020-02-18 14:00:00 INFO id=2 status=bar",
+                    "2020-02-16 12:34:58 INFO id=1 status=foo",
+                    "2020-02-17 23:59:00 WARN id=1 status=foo",
+                    "2020-02-04 23:59:00 WARN id=1 status=foo"
+                };
+
+            // 3.将原始日志转换为 LogInfo 
+            var parsedLogs = rawLogs
+                .Select(x =>
+                {
+                    var components = x.Split(' ');
+
+                    var date = components[0];
+                    var time = components[1];
+                    var level = components[2];
+                    var id = components[3].Split('=');
+                    var status = components[4].Split('=');
+
+                    var logInfo = new LogInfo
+                    {
+                        Contents =
+                        {
+                                {"level", level},
+                                {id[0], id[1]},
+                                {status[0], status[1]},
+                        },
+                        Time = DateTimeOffset.ParseExact($"{date} {time}", "yyyy-MM-dd HH:mm:ss", null)
+                    };
+
+                    return logInfo;
+                })
+                .ToList();
+            var logGroupInfo = new LogGroupInfo
+            {
+                Topic = "topic_test",
+                LogTags =
+                    {
+                        {"example", "true"},
+                    },
+                Logs = parsedLogs,
+                Source = "dq"
+            };
+
+            // 4.将日志发送给对应的日志库
+            string logstore = "net-logstore";
+            var response = client.PostLogStoreLogsAsync(new PostLogsRequest(logstore, logGroupInfo));
+
+            // 该方法无返回值必要，success仅为方便测试
+            return "success";
+        }
+
+
+    }
+}
